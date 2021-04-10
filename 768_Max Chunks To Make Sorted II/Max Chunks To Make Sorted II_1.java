8824e95d9999c3b31e2f151e239068d460f52c5c
michael54856 <michael548562@gmail.com>
2021-04-10 23:23:37 +0800

    update

--- 768_Max Chunks To Make Sorted II/Max Chunks To Make Sorted II_1.java
+++ 768_Max Chunks To Make Sorted II/Max Chunks To Make Sorted II_1.java
@@ -1,13 +1,43 @@
 class Solution {
-    public int missingNumber(int[] nums)
+    public int maxChunksToSorted(int[] arr)
     {
-        int len = nums.length;
-        int total = (1+len) * len / 2;
-        int sum = 0;
-        for(int i = 0; i < len; i++)
+        final int len = arr.length;
+        final int max_from_Left[] = new int[len];
+        final int min_from_right[] = new int[len];
+        
+        //record the maximum of the right sub array
+        int myMax = arr[0];
+        max_from_Left[0] = arr[0];
+        
+        
+        int myMin = arr[len-1];
+        min_from_right[len-1] = arr[len-1];
+        
+        for(int i = 1; i < len; i++)
         {
-        	sum += nums[i];
+        	if(arr[i] > myMax)
+        	{
+        		myMax = arr[i];
+        	}
+        	max_from_Left[i] = myMax;
         }
-        return total-sum;
+        
+        int ans = 1;
+        
+        for(int i = len-2; i >= 0; i--)
+        {
+        	//record the minimum of the left sub array
+        	if(arr[i] < myMin)
+        	{
+        		myMin = arr[i];
+        	}
+        	min_from_right[i] = myMin;
+        	if(max_from_Left[i] <= min_from_right[i+1])
+        	{
+        		ans++;
+        	}
+        }
+        return ans;
+ 
     }
 }
