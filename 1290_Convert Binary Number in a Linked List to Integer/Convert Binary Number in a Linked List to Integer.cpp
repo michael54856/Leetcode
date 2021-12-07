f6a4ff581a17561565bb4416c7c300e800816988
michael548562 <michael548562@gmail.com>
2021-12-07 10:21:26 +0800

    add 1290

---
+++ 1290_Convert Binary Number in a Linked List to Integer/Convert Binary Number in a Linked List to Integer.cpp
@@ -0,0 +1,15 @@
+class Solution
+{
+public:
+    int getDecimalValue(ListNode* head)
+    {
+        int ans = 0;
+        while(head)
+        {
+            ans <<= 1;
+            ans += head->val;
+            head = head->next;
+        }
+        return ans;
+    }
+};
